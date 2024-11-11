8257c686aba586899049b5b3aefd67fdb7f9643e
Rex342645 <vikasrautela88@gmail.com>
2024-11-11 22:47:40 +0530

    custom hooks

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+function App() {
+  
+
+  return (
+  <>
+  <h1 className="text-3xl text-center bg-orange-500 mt-4">hello</h1>
+  </>
+      
+  )
+}
+
+export default App
