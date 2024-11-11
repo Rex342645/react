2ad5d84444b8956ceb97c54b165700ff81e3b2b1
Rex342645 <vikasrautela88@gmail.com>
2024-11-11 22:55:24 +0530

    phase 1

--- currencyConvertor/hooks/customhook.js
+++
@@ -1,4 +0,0 @@
-import { useEffect,useState } from "react";
-function useCurrencyInfo(){
-
-}
--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
   <>
-  <h1 className="text-3xl text-center bg-orange-500 mt-4">hello</h1>
+  <h1 className="text-3xl text-center bg-orange-500 mt-96">hello</h1>
   </>
       
   )
