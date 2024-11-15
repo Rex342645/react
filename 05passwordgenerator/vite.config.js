a909b5d693c3f6183cfb108593d58acd60d1d71b
Rex342645 <vikasrautela88@gmail.com>
2024-11-15 21:26:11 +0530

    Fix deployment path to use subfolder for GitHub Pages

--- 05passwordgenerator/vite.config.js
+++ 05passwordgenerator/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react/05passwordgenerator/',
+  base: '/react/05passwordgenerator/',  
   build: {
-    outDir: 'dist', // Output directory after building
-    sourcemap: true, // Optional: generate sourcemaps for debugging
+    outDir: 'dist',  
+    sourcemap: true, 
   },
 })
