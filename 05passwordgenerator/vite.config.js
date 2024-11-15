35b25773deed05e9c756cad85df5bbcdf31c39db
Rex342645 <vikasrautela88@gmail.com>
2024-11-15 21:19:26 +0530

    Fix base path for GitHub Pages deployment

--- 05passwordgenerator/vite.config.js
+++ 05passwordgenerator/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react/05passwordgenerator/',
+  build: {
+    outDir: 'dist', // Output directory after building
+    sourcemap: true, // Optional: generate sourcemaps for debugging
+  },
 })
